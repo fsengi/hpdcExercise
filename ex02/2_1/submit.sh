e6074b0d4306975d667cf817fb3030ef3fbf198a
hpc 03 <hpc03@ceg-octane.ziti.uni-heidelberg.de>
2023-10-30 17:36:15 +0100

    move data 2_1 from florian folder

--- ex02/2_1/submit.sh
+++ ex02/2_1/submit.sh
@@ -4,11 +4,12 @@
 #SBATCH --job-name=HPDC03_job
 #SBATCH --partition=exercise_hpc   # partition (queue)
 #SBATCH -t 0-0:10              # time limit: (D-HH:MM) 
-#SBATCH --nodes=2              # number of nodes
-#SBATCH --ntasks-per-node=6    # number of cores
+#SBATCH --nodes=1              # number of nodes
+#SBATCH --ntasks-per-node=2    # number of cores
 #SBATCH --output=slurm.out     # file to collect standard output
 #SBATCH --error=slurm.err      # file to collect standard errors
 
 
 module load devtoolset/10 mpi/open-mpi-4.1.6
-mpirun ./test
+# mpirun ./test
+mpirun -host creek01,creek06,creek05,creek04 -np 4 ./test
