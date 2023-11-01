33561c4bb2ba5bda2d38bd90f52dcd25d1049625
hpc 03 <hpc03@ceg-octane.ziti.uni-heidelberg.de>
2023-11-01 16:21:05 +0100

    add barrier sync files

---
+++ ex02/Florian/submit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Resource Request
+
+## Resource Request
+#SBATCH --job-name=HPDC03_job
+#SBATCH --partition=exercise_hpc   # partition (queue)
+#SBATCH -t 0-0:10              # time limit: (D-HH:MM) 
+#SBATCH --nodes=1              # number of nodes
+#SBATCH --ntasks-per-node=2    # number of cores
+#SBATCH --output=slurm.out     # file to collect standard output
+#SBATCH --error=slurm.err      # file to collect standard errors
+
+module load devtoolset/10 mpi/open-mpi-4.1.6
+mpirun ./test
