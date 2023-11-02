8a6b27f09def1cff075c0eac522358dd1e681cbc
Florian.Schrittwiese <florian.schrittwieser@web.de>
2023-11-02 08:32:20 +0100

    florian add plot files in 2_1 and Florian

--- ex02/2_1/plot.py
+++ ex02/2_1/plot.py
@@ -1,22 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = """N;2;Runtime;0.588079;ms;Roundtrip;0.588079;ms;Average;294.039500;us
-N;4;Runtime;1.184967;ms;Roundtrip;1.184967;ms;Average;296.241750;us
-N;6;Runtime;2.183421;ms;Roundtrip;2.183421;ms;Average;363.903500;us
-N;8;Runtime;2.784112;ms;Roundtrip;2.784112;ms;Average;348.014000;us
-N;10;Runtime;104.488177;ms;Roundtrip;104.488177;ms;Average;10448.817700;us
-N;12;Runtime;97.703638;ms;Roundtrip;97.703638;ms;Average;8141.969833;us
-N;14;Runtime;100.221876;ms;Roundtrip;100.221876;ms;Average;7158.705429;us
-N;16;Runtime;108.087525;ms;Roundtrip;108.087525;ms;Average;6755.470312;us
-N;18;Runtime;150.884749;ms;Roundtrip;150.884749;ms;Average;8382.486056;us
-N;20;Runtime;187.796055;ms;Roundtrip;187.796055;ms;Average;9389.802750;us
-N;24;Runtime;195.186736;ms;Roundtrip;195.186736;ms;Average;8132.780667;us"""
+data_file = "D:\\Master\\HPDC\hpdcExercise\\ex02\\2_1\\data.txt"
+with open(data_file, "r") as file:
+    data = file.readlines()
 
 # Split the data into lines and parse values
-lines = data.split('\n')
-n_values = [int(line.split(';')[1]) for line in lines]
-average_values = [float(line.split(';')[-2][:-3]) for line in lines]
+n_values = [int(line.split(';')[1]) for line in data]
+average_values = [float(line.split(';')[-2][:-3]) for line in data]
 
 # Create a plot
 plt.figure(figsize=(10, 6))
