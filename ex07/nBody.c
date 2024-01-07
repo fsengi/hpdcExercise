1d1e5f983b22a0168d30f58d982dd604f6e00a1d
Florian <hpc03@ceg-octane.ziti.uni-heidelberg.de>
2024-01-07 15:38:44 +0100

    7_2 files

--- ex07/nBody.c
+++ ex07/nBody.c
@@ -2,72 +2,96 @@
 #include <stdlib.h>
 #include <mpi.h>
 #include <stddef.h>
+#include <math.h>
+#include <string.h>
+#include <sys/time.h>
 
+#define DELTA_T 5
 #define NBODY_DEFAULT 4 
-#define TIME_DEFAULT 2
+#define TIME_DEFAULT 100
+#define GAMMA -6.673*1e-11
+#define RAND_TH(a) RAND_MAX * a
+// #define DEBUG
 
-// typedef struct {
-//     double x;
-//     double y;
-//     double z;
-// }coordinate;
+//Struct for double3
+typedef struct
+{
+    double x, y, z;
+}double3;
+//Struct for double4
+typedef struct
+{
+    double x, y, z, w;
+}double4;
 
-typedef struct {
-    double mass;
-    double pos_x;
-    double pos_y;
-    double pos_z;
-    double v_x;
-    double v_y;
-    double v_z;
-    double a_x;
-    double a_y;
-    double a_z;
-}BodyProp;
+//Struct Definition for Bodies
+typedef struct
+{
+    int id;
+    double4 posMass; /* x = x */
+                    /* y = y */
+                    /* z = z */
+                    /* w = Mass */
+    double3 velocity;
+    /* x = v_x */
+    /* y = v_y */
+    /* z = v_z */
 
+    double3 force;
+
+} Body_t;
+
+void showData(Body_t* Bodys, int size);
+void showDataLog(char* string, Body_t* Bodys, int size, int rank);
+void showAllData(Body_t* Body, int size, int rank);
 
 int main(int argc ,char * argv[]) {
-    int number_of_processes, rank, nBody, timeIter, bodysPerThread;
+    int number_of_processes, rank, nBody, time, r, i, j;
+    int timeIter = 0;
     MPI_Status status;
     MPI_Request request;
-    BodyProp *Body = NULL;
-    BodyProp *subBody = NULL;
+    Body_t *Body = NULL;
+    Body_t *subBody = NULL;
+    struct timeval start, end;
 
     MPI_Init(&argc, &argv);
 
     MPI_Comm_size(MPI_COMM_WORLD, &number_of_processes);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    MPI_Barrier(MPI_COMM_WORLD);
     
-    /* create a type for struct car */
-    const int nitems=2;
-    int          blocklengths[10] = {1,1,1,1,1,1,1,1,1,1};
-    MPI_Datatype types[2] = {   MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, 
-                                MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE};
-    MPI_Datatype mpi_BodyProp;
-    MPI_Aint     offsets[10];
+    // setup MPI specific datatypes for custom struct
+    MPI_Datatype MPI_Double3;
+    MPI_Type_contiguous(3, MPI_DOUBLE, &MPI_Double3);
+    MPI_Type_commit(&MPI_Double3);
 
-    offsets[0] = offsetof(BodyProp, mass);
-    offsets[1] = offsetof(BodyProp, pos_x);
-    offsets[2] = offsetof(BodyProp, pos_y);
-    offsets[3] = offsetof(BodyProp, pos_z);
-    offsets[4] = offsetof(BodyProp, v_x);
-    offsets[5] = offsetof(BodyProp, v_y);
-    offsets[6] = offsetof(BodyProp, v_z);
-    offsets[7] = offsetof(BodyProp, a_x);
-    offsets[8] = offsetof(BodyProp, a_y);
-    offsets[9] = offsetof(BodyProp, a_z);
+    MPI_Datatype MPI_Double4;
+    MPI_Type_contiguous(4, MPI_DOUBLE, &MPI_Double4);
+    MPI_Type_commit(&MPI_Double4);
 
-    MPI_Type_create_struct(nitems, blocklengths, offsets, types, &mpi_BodyProp);
-    MPI_Type_commit(&mpi_BodyProp);
+    MPI_Datatype MPI_Body_t;
+    int blocklengths[4] = {1, 1, 1, 1};
+    MPI_Aint offsets[4];
+    offsets[0] = offsetof(Body_t, id);
+    offsets[1] = offsetof(Body_t, posMass);
+    offsets[2] = offsetof(Body_t, velocity);
+    offsets[3] = offsetof(Body_t, force);
 
-    
+    MPI_Datatype types[4] = {MPI_INT, MPI_Double4, MPI_Double3, MPI_Double3};
+    MPI_Type_create_struct(4, blocklengths, offsets, types, &MPI_Body_t);
+    MPI_Type_commit(&MPI_Body_t);
+
+    //Synchronisation
+    MPI_Barrier(MPI_COMM_WORLD);
+
     if(rank == 0)
     {
         // get args
         if (argc > 1) 
         {
-            nBody = atoi(argv[1]);  // Parse size from command line argument
+            // Parse size from command line argument
+            nBody = atoi(argv[1]);  
             timeIter = atoi(argv[2]);
         } 
         else 
@@ -75,50 +99,147 @@ int main(int argc ,char * argv[]) {
             nBody = NBODY_DEFAULT;
             timeIter = TIME_DEFAULT;
         }
-        
-        Body = (BodyProp*)malloc(nBody * sizeof(BodyProp));
-        int i;
-        for(i = 1; i < number_of_processes; i++)
-        {
-            MPI_Send(&nBody, sizeof(nBody)/sizeof(int), MPI_INT, i, 1, MPI_COMM_WORLD);
-            MPI_Send(&timeIter, sizeof(timeIter)/sizeof(int), MPI_INT, i, 2, MPI_COMM_WORLD);
-        }
+    }
+    //Broadcast number of Bodys and number of iterations
+    MPI_Bcast(&nBody, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&timeIter, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-        // initialisation 
+    //allocate Bodys 
+    Body = (Body_t*)malloc(nBody * sizeof(Body_t));
+
+    if(rank == 0)
+    {
+        // initialisation of Bodys
+        int i;
         for(i = 0; i < nBody; i++)
         {
-            Body[i].pos_x = (double)rand();
-            Body[i].pos_y = (double)rand();
-            Body[i].pos_z = (double)rand();
-            Body[i].mass = (double)rand();
-            Body[i].a_x = 0;
-            Body[i].a_y = 0;
-            Body[i].a_z = 0;
-            Body[i].v_x = 0;
-            Body[i].v_y = 0;
-            Body[i].v_z = 0;
+            Body[i].id = i;
+            Body[i].posMass.w = (double)rand() / RAND_TH(250000000);
+            Body[i].posMass.x = (double)rand() / RAND_TH(100);
+            Body[i].posMass.y = (double)rand() / RAND_TH(100);
+            Body[i].posMass.z = (double)rand() / RAND_TH(100);
+            Body[i].force.x = 0;
+            Body[i].force.y = 0;
+            Body[i].force.z = 0;
+            Body[i].velocity.x = 0;
+            Body[i].velocity.y = 0;
+            Body[i].velocity.z = 0;
         }
     }
-    else
+
+#ifdef DEBUG
+    showData(Body, nBody);
+#endif
+
+    int bodysPerThread = (int)(nBody / number_of_processes);
+    if(bodysPerThread == 0)
     {
-        MPI_Recv(&nBody, sizeof(nBody)/sizeof(int), MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
-        MPI_Recv(&timeIter, sizeof(timeIter)/sizeof(int), MPI_INT, MPI_ANY_SOURCE, 2, MPI_COMM_WORLD, &status);
+        printf("more processes than bodys");fflush(stdout);
+        return -1;
+    }
+    
+    //Broadcast Body Data to all ranks
+    MPI_Bcast(Body, nBody, MPI_Body_t, 0, MPI_COMM_WORLD);
+
+#ifdef DEBUG
+    showAllData(Body, bodysPerThread, rank);
+#endif
+    
+    //get starttime 
+    if(rank == 0)
+    {
+        gettimeofday(&start, NULL);
     }
 
-    bodysPerThread = nBody / number_of_processes;
-    subBody = (BodyProp*)malloc(bodysPerThread * sizeof(BodyProp));
-    
+    MPI_Barrier(MPI_COMM_WORLD);
 
-    //Scatter data
-    printf("size %d\n", sizeof(Body));
-    MPI_Scatter(Body, (bodysPerThread * sizeof(Body))/(sizeof(double)*10), subBody, mpi_BodyProp, (bodysPerThread * sizeof(Body))/(sizeof(double)*10) , mpi_BodyProp, 0, MPI_COMM_WORLD);
+    // loop for number of iterations
+    for(time = 0; time < timeIter; time++)
+    {
+        
+        // compute internal forces
+        for(j = rank*bodysPerThread; j < (rank+1)*bodysPerThread; j++)
+        {
+            //set force to zero from last iteration
+            Body[j].force.x = 0;
+            Body[j].force.y = 0;
+            Body[j].force.z = 0;
 
+            for(i = 0; i < nBody; i++)
+            {
+                if(Body[j].id == Body[i].id)
+                {
+                    //jump to next element if Body id are the same
+                    continue;
+                }
+                //calculate distance between Bodys
+                double rx = Body[j].posMass.x - Body[i].posMass.x;
+                double ry = Body[j].posMass.y - Body[i].posMass.y;
+                double rz = Body[j].posMass.z - Body[i].posMass.z;
+                double rdist_sq = rx * rx + ry * ry + rz * rz;
+                double rdist = sqrt(rdist_sq);
+                //normalize direction angle 
+                rx = rx / rdist;
+                ry = ry / rdist;
+                rz = rz / rdist;
+                //calculate forces
+                Body[j].force.x += ((GAMMA * Body[i].posMass.w * Body[j].posMass.w * rx) / rdist_sq);
+                Body[j].force.y += ((GAMMA * Body[i].posMass.w * Body[j].posMass.w * ry) / rdist_sq);
+                Body[j].force.z += ((GAMMA * Body[i].posMass.w * Body[j].posMass.w * rz) / rdist_sq);
+            }
 
-    printf("Body %lf ", subBody->pos_x);
-    // printSubGrid(subGridOld, size, rowsPerProcess, rank);
+            //calculate velocity for final object
+            Body[j].velocity.x = (Body[j].force.x  / Body[j].posMass.w) * DELTA_T;
+            Body[j].velocity.y = (Body[j].force.y  / Body[j].posMass.w) * DELTA_T;
+            Body[j].velocity.z = (Body[j].force.z  / Body[j].posMass.w) * DELTA_T;
+        
+            Body[j].posMass.x += Body[j].velocity.x * DELTA_T;
+            Body[j].posMass.y += Body[j].velocity.y * DELTA_T;
+            Body[j].posMass.z += Body[j].velocity.z * DELTA_T;
+
+        }
+        subBody = (Body_t*)malloc(bodysPerThread * sizeof(Body_t));
+        memcpy(subBody, &Body[rank*bodysPerThread], bodysPerThread * sizeof(Body_t));
+
+        //get all updated Bodys from other tasks 
+        MPI_Allgather(subBody, bodysPerThread, MPI_Body_t, Body, bodysPerThread, MPI_Body_t, MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+    //collect final data
+    MPI_Gather(&Body[rank*bodysPerThread], bodysPerThread, MPI_Body_t, Body, bodysPerThread, MPI_Body_t, 0, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if(rank == 0)
+    {
+        gettimeofday(&end, NULL);
+        double elapsed_time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1e6;
+        printf("Nbody,%d,numberofProcesses,%d,time,%lfs\n", nBody,number_of_processes,elapsed_time);fflush(stdout);
+        // showData(Body, nBody);
+    }
 
     MPI_Finalize();
-
-    free(subBody);
     free(Body);
+}
+
+void showData(Body_t* Bodys, int size)
+{
+    int a;
+    for(a = 0; a < size; a++)
+    {
+        printf("%d;%.1lf;%.1lf;%.1lf;", Bodys[a].id, Bodys[a].posMass.x, Bodys[a].posMass.y, Bodys[a].posMass.z);
+    }
+    printf("\n");
+}
+
+void showAllData(Body_t* Body, int size, int rank)
+{
+    int a;
+    for(a = 0; a < rank*size; a++)
+    {
+        printf("id %d pos_x %.1f pos_y %.1f pos_z %.1f v_x %.1f v_y %.1f v_z %.1f m %.1f F_x %.1f F_y %.1f F_z %.1f\n", 
+        Body[a].id, Body[a].posMass.x, Body[a].posMass.y, Body[a].posMass.z,
+        Body[a].velocity.x, Body[a].velocity.y, Body[a].velocity.z,Body[a].posMass.w,
+        Body[a].force.x, Body[a].force.y, Body[a].force.z);
+    }
+    printf("\n\n");
 }
--- ex07/submit.sh
+++ ex07/submit.sh
@@ -1,21 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 
-## Resource Request for One Node
-#SBATCH --job-name=mmul_sequential
+#SBATCH --job-name=nBody
 #SBATCH --partition=exercise_hpc   # partition (queue)
-#SBATCH -t 0-00:10              # time limit: (D-HH:MM) 
-#SBATCH --nodes=1              # number of nodes
-#SBATCH --ntasks-per-node=1
-#SBATCH --output=mmul.out     # file to collect standard output
-#SBATCH --error=mmul.err      # file to collect standard errors
+#SBATCH -t 0-1:00             # time limit: (D-HH:MM)
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --output=nBody.out     # file to collect standard output
+#SBATCH --error=nBody.err      # file to collect standard errors
 
-# nodes=(1 2)
-# nodes=(2)
-# Define an array of tasks per node configurations (e.g., tasks_per_node=(4 8 16))
-# tasks_per_node=( 2 4 6 7 8 9 10 11 12 14 16)
-# tasks_per_node=( 2 )
 
-module load devtoolset/10 mpi/open-mpi-4.0.5
-mpicc -o bin/nBody nBody.c
+# 2 4 8 12 24 48 64
+nodes=(1 1 1 1 2 3 4 5 6)
+tasks_per_node=(2 4 8 12 24 36 48 60 72)
 
-srun ./bin/nBody >> data.txt
+rm finaldata.txt
+rm data1.txt
+
+length=${#nodes[@]}
+# module load devtoolset/10 mpi/open-mpi-4.1.6
+mpicc -o bin/nBody nBody.c -lm
+
+for (( j = 0; j < length; j++ )) ; do
+        for (( i = 128; i <= 32768; i*=2 )) ; do
+                echo "${tasks_per_node[$j]} Processes,${nodes[$j]} Nodes, $i Bodys" >> data1.txt
+                srun --partition=exercise_hpc --nodes=${nodes[$j]} --ntasks=${tasks_per_node[$j]} ./bin/nBody $i 100 >> finaldata.txt
+        done
+done 
